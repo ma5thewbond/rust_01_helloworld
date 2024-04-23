e245e94e31e25348ac1acde37d2b3bc2e2ca417d
ma5thew <martin.barcansky@datapac.sk>
2024-04-23 19:11:45 +0200

    use command line params

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
-    println!("Hello, rust!");
+    let mut args = env::args();
+    args.next().unwrap();
+    let greeting = args.next().unwrap_or("Hello".to_string());
+    let name = args.next().unwrap_or("Rust".to_string());
+    println!("{greeting}, {name}!");
 }
