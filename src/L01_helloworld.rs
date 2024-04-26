12fb949ad96750370d078f49bdd062a2d77cbd56
ma5thew <martin.barcansky@datapac.sk>
2024-04-27 00:13:43 +0200

    Lesson 2, cmd params and input string manipulation

---
+++ src/L01_helloworld.rs
@@ -0,0 +1,10 @@
+// Lesson 1 - Hello world
+/*
+fn main() {
+    let mut args = env::args();
+    args.next().unwrap();
+    let greeting = args.next().unwrap_or("Hello".to_string());
+    let name = args.next().unwrap_or("Rust".to_string());
+    println!("{greeting}, {name}!");
+}
+*/
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,60 @@
+use slug::slugify;
 use std::env;
+use std::io;
 
 fn main() {
-    let mut args = env::args();
-    args.next().unwrap();
-    let greeting = args.next().unwrap_or("Hello".to_string());
-    let name = args.next().unwrap_or("Rust".to_string());
-    println!("{greeting}, {name}!");
+    let args: Vec<String> = env::args().collect();
+    let mut input = String::new();
+    println!("Enter text to modify: ");
+    io::stdin().read_line(&mut input).unwrap();
+
+    if args.len() == 1 {
+        println!("Original: {input}");
+    }
+
+    if args[1] == "lowercase" {
+        println!("lowercase: {}", input.to_lowercase());
+    }
+
+    if args[1] == "uppercase" {
+        println!("UPPERCASE: {}", input.to_uppercase());
+    }
+
+    if args[1] == "no-spaces" {
+        println!("nospaces: {}", input.replace(" ", ""));
+    }
+
+    if args[1] == "slugify" {
+        let mut slug = input.clone();
+        slug = slugify(slug);
+        println!("slugify: {}", slug);
+    }
+
+    if args[1] == "reverse" {
+        print!("esrever: ");
+        for x in input.chars().rev() {
+            print!("{x}");
+        }
+        println!("");
+    }
+
+    if args[1] == "trim" {
+        println!("trim: {}", input.trim());
+    }
+
+    if args[1] == "all" {
+        println!("Original: {input}");
+        println!("lowercase: {}", input.to_lowercase());
+        println!("UPPERCASE: {}", input.to_uppercase());
+        println!("nospaces: {}", input.replace(" ", ""));
+        let mut slug = input.clone();
+        slug = slugify(slug);
+        println!("slugify: {}", slug);
+        print!("esrever: ");
+        for x in input.chars().rev() {
+            print!("{x}");
+        }
+        println!("");
+        println!("trim: {}", input.trim());
+    }
 }
